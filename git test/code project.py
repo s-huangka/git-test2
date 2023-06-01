501df9e8132ea8f7ce9de834e3d4f0e79ac9067a
laurendex <133685058+laurendex@users.noreply.github.com>
2023-06-01 10:19:03 -0700

    added comments and debugged

--- git test/code project.py
+++ git test/code project.py
@@ -3,18 +3,25 @@ Code Your Own Project: (put project idea here)
 Kaylee Huang and Lauren Dex
 """
 
-import pip._vendor.requests
+# libraries and variables
+from pip._vendor import requests
 
 BASE_URL = "https://cdn2.thecatapi.com/v1/images/search"
 
-num = input("how many cat pictures do you want to see? (please enter a number between 1 and 10) ")
+# user input
+num = int(input("how many cat pictures do you want to see? (please enter a number between 1 and 10) "))
 
 if num > 10:
     num = input("please enter a number between 1 and 10: ")
+
+# first API call
 url = BASE_URL + "?limit=" + str(num)
 
 response = requests.get(url)
+
 if response.status_code != 200:
     print("error: " + str(response.status_code))
 data = response.json()
 
+url=data["url"]
+print(url)
