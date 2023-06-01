1399a9f09775d4dbae2a1beea67e4bac67e0fe9a
s-huangka <133685166+s-huangka@users.noreply.github.com>
2023-06-01 09:48:20 -0700

    added API/json code

--- git test/code project.py
+++ git test/code project.py
@@ -3,3 +3,15 @@ Code Your Own Project: (put project idea here)
 Kaylee Huang and Lauren Dex
 """
 
+import requests
+
+BASE_URL = "https://cdn2.thecatapi.com/v1/images/search"
+
+
+
+url = BASE_URL + 
+
+response = requests.get(url)
+if response.status_code != 200:
+    print("error: " + str(response.status_code))
+data = response.json()
