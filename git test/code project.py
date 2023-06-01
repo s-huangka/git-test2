b4bee16bd3c2f5c2e486f36ed9439c4d8cc04d53
s-huangka <133685166+s-huangka@users.noreply.github.com>
2023-06-01 10:43:14 -0700

    beginning user ID code

--- git test/code project.py
+++ git test/code project.py
@@ -11,10 +11,11 @@ all_IDs = []
 BASE_URL = "https://api.thecatapi.com/v1/images/search"
 
 # user input
-#new_image = input("enter \"n\" to generate a new image: ") == "n"
-"""
-if new_image:
+new_image = input("enter \"n\" to generate a new image or \"o\" to generate the most recent image: ") 
+
+if new_image == "n":
     url = BASE_URL + "?limit=1"
+    print(url)
 
     response = requests.get(url)
 
@@ -22,28 +23,14 @@ if new_image:
         print("error: " + str(response.status_code))
         exit()
     data = response.json()
+    print(data)
 
-    url=data["url"]
+    url=data[0]["url"]
     print(url)
-    new_ID = data["id"]
+
+    new_ID = data[0]["id"]
     all_IDs.append(new_ID)
     print(all_IDs)
-    """
 
-url = BASE_URL + "?limit=1"
-print(url)
-
-response = requests.get(url)
-
-if response.status_code != 200:
-    print("error: " + str(response.status_code))
-    exit()
-data = response.json()
-print(data)
-
-url=data[0]["url"]
-print(url)
-
-new_ID = data[0]["id"]
-all_IDs.append(new_ID)
-print(all_IDs)
+elif new_image == "o":
+    url = BASE_URL
