8764c736e2452768b54e60e0cb9dfded88e85829
s-huangka <133685166+s-huangka@users.noreply.github.com>
2023-06-01 09:58:52 -0700

    edited API code

--- git test/code project.py
+++ git test/code project.py
@@ -7,11 +7,12 @@ import requests
 
 BASE_URL = "https://cdn2.thecatapi.com/v1/images/search"
 
+num = input("how many cat pictures do you want to see? (please enter a number between 1 and 100) ")
 
-
-url = BASE_URL + 
+url = BASE_URL + "?limit=" + str(num)
 
 response = requests.get(url)
 if response.status_code != 200:
     print("error: " + str(response.status_code))
 data = response.json()
+
