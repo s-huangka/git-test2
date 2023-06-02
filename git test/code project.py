5f911ba04d61ca298aa03b1549a4fd75ec7ff262
laurendex <133685058+laurendex@users.noreply.github.com>
2023-06-02 10:59:15 -0700

    added code for second API call

--- git test/code project.py
+++ git test/code project.py
@@ -3,9 +3,10 @@ Code Your Own Project: Random Cat Images
 Kaylee Huang and Lauren Dex
 """
 
-# libraries and variables
+# Libraries and variables
 from pip._vendor import requests
 import pickle
+import pprint
 from os import path
 
 def save(object, file):
@@ -28,17 +29,22 @@ while True:
 
     print()
     id = input("Enter \"n\" to generate a new image, \"o\" to generate the most recent image, or \"q\" to quit: ").capitalize()
+    
+    # Quit
     if id == "Q":
         break
     
+    # Generate the most recent image
     elif id == "O" and len(all_IDs) >= 1:
+        
+        # First API call
         url = BASE_URL + all_IDs[-1]
         print(url)
 
         response = requests.get(url)
 
         if response.status_code != 200:
-            print("error: " + str(response.status_code))
+            print("ERROR: " + str(response.status_code))
             exit()
         data = response.json()
         print(data)
@@ -46,28 +52,69 @@ while True:
         url=data["url"]
         print(url)
 
+        # Second API call
+        response = requests.get(url)
+
+        if response.status_code != 200:
+            print("ERROR: " + str(response.status_code))
+            exit()
+        data = response.json()
+        print(data)
+
+
+
+    # Failure to generate the most recent image
     elif id == "O" and len(all_IDs) < 1:
-        print("you must generate a new image before calling this function")
+        print("You must generate a new image before calling this function")
         exit()
-        
+
+    # Generate a new image
     elif id == "N":
+
+        # First API call
         url = BASE_URL + "search?limit=1"
         print(url)
 
         response = requests.get(url)
 
         if response.status_code != 200:
-            print("error: " + str(response.status_code))
+            print("ERROR: " + str(response.status_code))
             exit()
         data = response.json()
         print(data)
 
-        url=data[0]["url"]
-        print(url)
+        url = data[0]["url"]
+        print("Ctrl + click this link to view your random cat image:", url)
 
         new_ID = data[0]["id"]
         all_IDs.append(new_ID)
         print(all_IDs)
 
+        # Second API call
+        url = BASE_URL + new_ID
+        print(url)
+
+        response = requests.get(url)
+
+        if response.status_code != 200:
+            print("ERROR: " + str(response.status_code))
+            exit()
+        data = response.json()
+        print(data)
+        if "breeds" in data:
+            print(data["breeds"])
+        if "width" in data:
+            print("Width:", data["width"])
+        
+        #if "description" in data:   
+            #print("Description:", data["breeds"]["description"])
+
+        #if "origin" in data:
+            #print("Origin:", data["breeds"]["origin"])
+
+        #if "temperament" in data:
+            #print("Temperament:", data["breeds"]["temperament"])
+
+    # User entered invalid input
     else:
         exit()
